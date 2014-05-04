eb85c78b301623ec6e20445f2c492c5efb8a4015
Steven Nunez <steven.nunez@gmail.com>
2014-05-04 13:14:39 -0400

    Use blocks

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -1,19 +1,21 @@
-def caesar_encode(string, offset)
+def caesar(string, offset, &block)
   alpha = ('a'..'z')
   translation = alpha.cycle.lazy.take(offset + 26).to_a[offset...(offset + 26)]
   alpha_range = "#{alpha.first}-#{alpha.last}"
   translation_range = "#{translation.first}-za-#{translation.last}"
   upper_alpha_range = "#{alpha.first.upcase}-#{alpha.last.upcase}"
   upper_translation_range = "#{translation.first.upcase}-ZA-#{translation.last.upcase}"
-  string.tr(alpha_range, translation_range).tr(upper_alpha_range, upper_translation_range)
+  block.call(alpha_range, translation_range, upper_alpha_range, upper_translation_range)
+end
+
+def caesar_encode(string, offset)
+  caesar(string, offset) do |alpha_range, translation_range, upper_alpha_range, upper_translation_range|
+    string.tr(alpha_range, translation_range).tr(upper_alpha_range, upper_translation_range)
+  end
 end
 
 def caesar_decode(encoded_string, offset)
-  alpha = ('a'..'z')
-  translation = alpha.cycle.lazy.take(offset + 26).to_a[offset...(offset + 26)]
-  alpha_range = "#{alpha.first}-#{alpha.last}"
-  translation_range = "#{translation.first}-za-#{translation.last}"
-  upper_alpha_range = "#{alpha.first.upcase}-#{alpha.last.upcase}"
-  upper_translation_range = "#{translation.first.upcase}-ZA-#{translation.last.upcase}"
-  encoded_string.tr(translation_range, alpha_range).tr(upper_translation_range, upper_alpha_range)
+  caesar(encoded_string, offset) do |alpha_range, translation_range, upper_alpha_range, upper_translation_range|
+    encoded_string.tr(translation_range, alpha_range).tr(upper_translation_range, upper_alpha_range)
+  end
 end
