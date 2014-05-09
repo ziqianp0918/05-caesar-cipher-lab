1e091de9c753e78f071a67b0cf0b77ad433b8c1a
Steven Nunez <steven.nunez@gmail.com>
2014-05-09 15:09:51 -0400

    Update solution

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -1,10 +1,14 @@
 def caesar(string, offset, &block)
-  alpha = ('a'..'z')
-  translation = alpha.cycle.lazy.take(offset + 26).to_a[offset...(offset + 26)]
-  alpha_range = "#{alpha.first}-#{alpha.last}"
-  translation_range = "#{translation.first}-za-#{translation.last}"
-  upper_alpha_range = "#{alpha.first.upcase}-#{alpha.last.upcase}"
-  upper_translation_range = "#{translation.first.upcase}-ZA-#{translation.last.upcase}"
+  return string if (offset % 26).zero?
+
+  translation = alphabet_translation(offset)
+
+  alpha_range = to_range(alphabet)
+  translation_range = to_range(translation)
+
+  upper_alpha_range = alpha_range.upcase
+  upper_translation_range = translation_range.upcase
+
   block.call(alpha_range, translation_range, upper_alpha_range, upper_translation_range)
 end
 
@@ -18,4 +22,27 @@ def caesar_decode(encoded_string, offset)
   caesar(encoded_string, offset) do |alpha_range, translation_range, upper_alpha_range, upper_translation_range|
     encoded_string.tr(translation_range, alpha_range).tr(upper_translation_range, upper_alpha_range)
   end
+end
+
+# Helpers
+def to_range(letters_array)
+  if letters_array.first == 'a'
+    "a-z"
+  elsif letters_array.first == 'z'
+    "za-y"
+  else
+    "#{letters_array.first}-za-#{letters_array.last}"
+  end
+end
+
+def alphabet_translation(offset)
+  alphabet.cycle.take(offset + 26).to_a[offset...(offset + 26)]
+end
+
+def alphabet
+  [*'a'..'z']
+end
+
+def alpha_range
+  "#{alphabet.first}-#{alphabet.last}"
 end
